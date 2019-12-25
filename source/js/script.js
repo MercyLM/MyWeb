c681b6ec11599c9de962faa8c96e75926b31e9aa
MercyLM <MercyLM@users.noreply.github.com>
2019-12-25 19:12:06 +0800

    12/25

--- source/js/script.js
+++ source/js/script.js
@@ -9,25 +9,23 @@ window.onload = function () {
 function myFunction() {
 
     var navbar = document.getElementById("myNavbar");
-    var drop1 = document.getElementById("myDrop1");
-    var drop2 = document.getElementById("myDrop2");
-    var drop3 = document.getElementById("myDrop3");
-    var drop4 = document.getElementById("myDrop4");
+    var drop = document.getElementsByClassName("dropdown-content");
 
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    if (document.body.scrollTop > 435 || document.documentElement.scrollTop > 435) {
 
         navbar.className = "nav-title" + " nav-title-v1";
-        drop1.className = "dropdown-content" + " dropdown-content-v1";
-        drop2.className = "dropdown-content" + " dropdown-content-v1";
-        drop3.className = "dropdown-content" + " dropdown-content-v1";
-        drop4.className = "dropdown-content" + " dropdown-content-v1";
-
+        
+        for ( i = 0 ; i < drop.length ; i++){
+            
+            drop[i].className = "dropdown-content" + " dropdown-content-v1";
+        }
     } else {
-
+        
         navbar.className = navbar.className.replace(" nav-title-v1", "");
-        drop1.className = drop1.className.replace(" dropdown-content-v1", "");
-        drop2.className = drop2.className.replace(" dropdown-content-v1", "");
-        drop3.className = drop3.className.replace(" dropdown-content-v1", "");
-        drop4.className = drop4.className.replace(" dropdown-content-v1", "");
+        
+        for ( i = 0 ; i < drop.length ; i++){
+            
+            drop[i].className = drop[i].className.replace(" dropdown-content-v1", "");
+        }
     }
 }
