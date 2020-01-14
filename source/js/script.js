99501de6f7bf52c8f3a03968dd1bd413c17b577e
MercyLM <MercyLM@users.noreply.github.com>
2020-01-14 14:42:11 +0800

    01/14

--- source/js/script.js
+++ source/js/script.js
@@ -8,24 +8,24 @@ window.onload = function () {
 
 function myFunction() {
 
-    var navbar = document.getElementById("myNavbar");
+    var navbar = document.getElementById("NavBar");
     var drop = document.getElementsByClassName("dropdown-content");
 
-    if (document.body.scrollTop > 435 || document.documentElement.scrollTop > 435) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
 
-        navbar.className = "nav-title" + " nav-title-v1";
+        navbar.classList.add("sticky");
         
-        for ( i = 0 ; i < drop.length ; i++){
-            
-            drop[i].className = "dropdown-content" + " dropdown-content-v1";
-        }
+//        for ( i = 0 ; i < drop.length ; i++){
+//            
+//            drop[i].className = "dropdown-content" + " dropdown-content-v1";
+//        }
     } else {
         
-        navbar.className = navbar.className.replace(" nav-title-v1", "");
+        navbar.classList.remove("sticky");
         
-        for ( i = 0 ; i < drop.length ; i++){
-            
-            drop[i].className = drop[i].className.replace(" dropdown-content-v1", "");
-        }
+//        for ( i = 0 ; i < drop.length ; i++){
+//            
+//            drop[i].className = drop[i].className.replace(" dropdown-content-v1", "");
+//        }
     }
 }
