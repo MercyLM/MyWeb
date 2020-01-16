3ded9ad72c23133122a5b3526969405236434c84
MercyLM <MercyLM@users.noreply.github.com>
2020-01-16 21:20:34 +0800

    01/16

--- source/js/index.js
+++ source/js/index.js
@@ -1,8 +1,8 @@
 window.onload = function () {
 
     //資源載入
-    $("#navBar").load("../source/navbar.html");
-    $("#footer").load("../source/footer.html");
+//    $("#navBar").load("../source/navbar.html");
+//    $("#footer").load("../source/footer.html");
     //初始化
     var timer = null;
     var timerChack = true;
