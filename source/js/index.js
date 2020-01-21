a70a0dbf63ee7b602176619534ba68ebc402c3f5
MercyLM <MercyLM@users.noreply.github.com>
2020-01-21 20:05:56 +0800

    01/21

--- source/js/index.js
+++ source/js/index.js
@@ -52,26 +52,17 @@ function myFunction() {
 
     var navbar = document.getElementById("nav");
     var topbtn = document.getElementById("top-btn");
-    //    var drop = document.getElementsByClassName("dropdown-content");
 
     if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
 
         navbar.classList.add("sticky");
         topbtn.classList.add("show");
 
-        //        for ( i = 0 ; i < drop.length ; i++){
-        //            
-        //            drop[i].className = "dropdown-content" + " dropdown-content-v1";
-        //        }
     } else {
 
         navbar.classList.remove("sticky");
         topbtn.classList.remove("show");
 
-        //        for ( i = 0 ; i < drop.length ; i++){
-        //            
-        //            drop[i].className = drop[i].className.replace(" dropdown-content-v1", "");
-        //        }
     }
 }
 
