e397b456fba20c1192edafce4dd4d01af6bcb59f
MercyLM <MercyLM@users.noreply.github.com>
2020-02-03 20:21:31 +0800

    02/03

--- source/js/index.js
+++ source/js/index.js
@@ -1,88 +1,92 @@
-$(function(){
-    
-    $("#nav").load("./source/navbar.html");
-    $("#footer").load("./source/footer.html");
-});
+//$(function(){
+//    
+//    $("#nav").load("../source/navbar.html");
+//    $("#footer").load("../source/footer.html");
+//});
 
 window.onload = function () {
 
-    //載入
-//    $("#nav").load("./source/navbar.html");
-//    $("#footer").load("./source/footer.html");
-    //初始化
-    var timer = null;
-    var timerChack = true;
-    //物件預載
-    var btn = document.getElementById("top-btn");
-    var menu = document.getElementById("menu");
-    var menuBox = document.getElementById("menu-box");
-    var menuClose = document.getElementById("close-menu");
-
-    menuFunction(menu, menuBox, menuClose)
-
-    window.onscroll = function () {
-
-        myFunction()
-
-        var top = document.documentElement.scrollTop || document.body.scrollTop;
-
-        if (!timerChack) {
-
-            console.log("stop");
-            clearInterval(timer);
-        }
-        timerChack = false;
-
-        btn.onclick = function () {
-
-            timer = setInterval(function () {
-
-                var top = document.documentElement.scrollTop || document.body.scrollTop;
-                var speed = Math.floor(-top / 6);
-
-                document.documentElement.scrollTop = document.body.scrollTop = top + speed;
-
-                timerChack = true;
-
-                if (top == 0) {
-
-                    console.log("clear");
-                    clearInterval(timer);
-                }
-            }, 30);
-        }
-    };
+//    //初始化
+//    var timer = null;
+//    var timerChack = true;
+//    //物件預載
+//    var btn = document.getElementById("top-btn");
+////    var menu = document.getElementById("menu");
+////    var menuBox = document.getElementById("menu-box");
+////    var menuClose = document.getElementById("close-menu");
+//
+//    window.onscroll = function () {
+//
+//        myFunction()
+//
+//        var top = document.documentElement.scrollTop || document.body.scrollTop;
+//
+//        if (!timerChack) {
+//
+//            console.log("stop");
+//            clearInterval(timer);
+//        }
+//        timerChack = false;
+//
+//        btn.onclick = function () {
+//
+//            timer = setInterval(function () {
+//
+//                var top = document.documentElement.scrollTop || document.body.scrollTop;
+//                var speed = Math.floor(-top / 6);
+//
+//                document.documentElement.scrollTop = document.body.scrollTop = top + speed;
+//
+//                timerChack = true;
+//
+//                if (top == 0) {
+//
+//                    console.log("clear");
+//                    clearInterval(timer);
+//                }
+//            }, 30);
+//        }
+//    };
+    
+//    menuFunction(menu, menuBox, menuClose)
 };
 
-function myFunction() {
-
-    var navbar = document.getElementById("nav");
-    var topbtn = document.getElementById("top-btn");
-
-    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-
-        navbar.classList.add("sticky");
-        topbtn.classList.add("show");
-
-    } else {
-
-        navbar.classList.remove("sticky");
-        topbtn.classList.remove("show");
-
-    }
-}
-
-function menuFunction(menu, menuBox, menuClose) {
-
-    menu.onclick = function () {
-
-        menuBox.style.width = "100%";
-        menu.style.display = "none";
-    }
-
-    menuClose.onclick = function () {
-
-        menuBox.style.width = "0%";
-        menu.style.display = "inline-block";
-    }
-}
+//function myFunction() {
+//
+//    var navbar = document.getElementById("nav");
+//    var topbtn = document.getElementById("top-btn");
+//
+//    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+//
+//        navbar.classList.add("sticky");
+//        topbtn.classList.add("show");
+//
+//    } else {
+//
+//        navbar.classList.remove("sticky");
+//        topbtn.classList.remove("show");
+//
+//    }
+//}
+//
+//function menuFunction() {
+//    
+//    var menu = document.getElementById("menu");
+//    var menuBox = document.getElementById("menu-box");
+//    var menuClose = document.getElementById("close-menu");
+//    var aside = document.getElementById("aside");
+//
+//    menu.onclick = function () {
+//
+//        menuBox.style.width = "100%";
+//        menu.style.display = "none";
+//        aside.style.zIndex = 0;
+//    }
+//
+//    menuClose.onclick = function () {
+//
+//        menuBox.style.width = "0%";
+//        menu.style.display = "inline-block";
+//        aside.style.zIndex = 4;
+//    }
+//}
---
+++ source/js/main.js
@@ -0,0 +1,118 @@
+$(function () {
+
+    $("#aside").load("../source/aside.html");
+    $("#footer").load("../source/footer.html");
+    $("#nav").load("../source/navbar.html", function (responseTxt, statusTxt, xhr) {
+
+        if (statusTxt == "success") {
+
+            console.log("navbar載入完成！");
+            menuFunction();
+            scrollFunction();
+        }
+    });
+});
+
+function scrollFunction() {
+
+    //初始化
+    var timer = null;
+    var timerChack = true;
+    //物件預載
+    var btn = document.getElementById("top-btn");
+    var navbar = document.getElementById("nav");
+    var topbtn = document.getElementById("top-btn");
+
+    //檢測scroll位置
+    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+
+        navbar.classList.add("sticky");
+        topbtn.classList.add("show");
+
+    } else {
+
+        navbar.classList.remove("sticky");
+        topbtn.classList.remove("show");
+
+    }
+
+    //監聽滾動
+    window.onscroll = function () {
+
+        myFunction(navbar, topbtn)
+
+        var top = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (!timerChack) {
+
+            console.log("stop");
+            clearInterval(timer);
+        }
+        timerChack = false;
+
+        btn.onclick = function () {
+
+            timer = setInterval(function () {
+
+                var y = document.documentElement.scrollTop || document.body.scrollTop;
+                var speed = Math.floor(-y / 6);
+
+                document.documentElement.scrollTop = document.body.scrollTop = y + speed;
+
+                timerChack = true;
+
+                if (y == 0) {
+
+                    console.log("clear");
+                    clearInterval(timer);
+                }
+            }, 30);
+        }
+    };
+}
+
+function myFunction(navbar, topbtn) {
+
+    if (document.getElementById("index")) {
+
+        //        console.log("這是首頁");
+
+        if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+
+            navbar.classList.add("sticky");
+            topbtn.classList.add("show");
+
+        } else {
+
+            navbar.classList.remove("sticky");
+            topbtn.classList.remove("show");
+
+        }
+    } else {
+
+        //        console.log("這不是首頁");
+    }
+
+}
+
+function menuFunction() {
+
+    var menu = document.getElementById("menu");
+    var menuBox = document.getElementById("menu-box");
+    var menuClose = document.getElementById("close-menu");
+    var aside = document.getElementById("aside");
+
+    menu.onclick = function () {
+
+        menuBox.style.width = "100%";
+        menu.style.display = "none";
+        aside.style.zIndex = 0;
+    }
+
+    menuClose.onclick = function () {
+
+        menuBox.style.width = "0%";
+        menu.style.display = "inline-block";
+        aside.style.zIndex = 4;
+    }
+}
